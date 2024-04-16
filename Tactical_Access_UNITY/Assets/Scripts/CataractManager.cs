8589bf4ef05d795174bbaf7f651074bdae536cff
Reece Dyke <reece2.dyke@live.uwe.ac.uk>
2024-04-16 13:15:38 +0000

    Added brightness,contrast and satuation.
    Cataract scene complete. Next is Glaucoma

--- Tactical_Access_UNITY/Assets/Scripts/CataractManager.cs
+++ Tactical_Access_UNITY/Assets/Scripts/CataractManager.cs
@@ -2,18 +2,39 @@ using UnityEngine;
 using UnityEngine.Profiling;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class CataractManager : MonoBehaviour
 {
     public Volume volume;
-    public DepthOfField DepthOfField;
+    private DepthOfField DepthOfField;
     public int currentNo = 0;
+
+    [SerializeField] private Slider BrightnessSlider;
+    [SerializeField] private float maxSliderAmount = 100f;
+    private ColorAdjustments colorAdjustments;
+
+    [SerializeField] private Slider ContrastSlider;
+    [SerializeField] private Slider SaturationSlider;
+
 
     private void Update()
     {
         if (volume.profile.TryGet(out DepthOfField depthOfField))
         {
             UpdateDepthOfField(depthOfField);
+        }
+    }
+
+    private void Start()
+    {
+        if (volume.profile.TryGet(out ColorAdjustments ca))
+        {
+            colorAdjustments = ca;
+        }
+        else
+        {
+            Debug.LogError("Failed to get ColorAdjustments from volume profile.");
         }
     }
 
@@ -54,5 +75,23 @@ public class CataractManager : MonoBehaviour
     public void SetCurrentNo(int newValue)
     {
         currentNo = newValue;
+    }
+
+    public void BrightnessSliderChange(float value)
+    {
+        float localValue = value * maxSliderAmount;
+        colorAdjustments.postExposure.value = localValue / 100;
+    }
+
+    public void ContrastSliderChange(float value)
+    {
+        float localValue = value * maxSliderAmount;
+        colorAdjustments.contrast.value = localValue / 100;
+    }
+
+    public void SaturationSliderChange(float value)
+    {
+        float localValue = value * maxSliderAmount;
+        colorAdjustments.saturation.value = localValue / 100;
     }
 }
